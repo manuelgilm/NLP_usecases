a4f0af8ba3d2ea7a4c52829b9e5896bdd04e722d
Manuel Gil <manuelgilsitio@gmail.com>
2024-02-03 10:16:10 -0500

    adding optimizer

--- sentiment_analysis/sentiment_analysis/entrypoints/train.py
+++ sentiment_analysis/sentiment_analysis/entrypoints/train.py
@@ -10,8 +10,10 @@ from sklearn.model_selection import train_test_split
 # fmt: off
 from sentiment_analysis.data_preparation.retrieval import get_train_and_test_data  # noqa
 from sentiment_analysis.training.mlflow_utils import get_or_create_mlflow_experiment  # noqa
+from sentiment_analysis.training.mlflow_utils import log_classification_metrics  # noqa
 from sentiment_analysis.training.optimize import get_search_space
 from sentiment_analysis.training.optimize import objective_function
+from sentiment_analysis.training.training_pipelines import get_transformer_pipeline  # noqa
 from sentiment_analysis.utils.utils import get_config
 
 # fmt: on
@@ -54,8 +56,21 @@ def train():
             ),
             space=search_space,
             algo=tpe.suggest,
-            max_evals=10,
+            max_evals=100,
             trials=Trials(),
             show_progressbar=True,
         )
+
+        pipeline = get_transformer_pipeline(
+            model_prefix=model_prefix, text_columns=["sentence"]
+        )
+        pipeline.set_params(**best_params)
+
+        pipeline.fit(train_df.drop(["label"], axis=1), train_df["label"])
+        predictions = pipeline.predict(test_df.drop(["label"], axis=1))
+        _ = log_classification_metrics(
+            predictions, test_df["label"], run.info.run_id
+        )
+        mlflow.sklearn.log_model(pipeline, "model")
+
     return best_params
