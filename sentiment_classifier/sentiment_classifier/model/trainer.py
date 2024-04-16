5d677ff2c19d596a98da8bba84b147591051d7f5
Manuel Gil <manuelgilsitio@gmail.com>
2024-04-15 22:59:00 -0500

    adding configs and model alias

--- sentiment_classifier/sentiment_classifier/model/trainer.py
+++ sentiment_classifier/sentiment_classifier/model/trainer.py
@@ -9,6 +9,7 @@ from spacy.cli.train import train
 from sentiment_classifier.model.mlflow_utils import get_or_create_experiment
 from sentiment_classifier.utils.utils import get_root_dir
 from sentiment_classifier.utils.utils import load_spacy_model
+from sentiment_classifier.utils.utils import read_config
 
 
 def train_spacy_model(
@@ -26,34 +27,31 @@ def train_spacy_model(
     :param n_iter: Number of iterations.
     :return: None
     """
+    configs = read_config("train_config")
+    mlflow_conf = configs["mlflow"]
+    
     root_dir = get_root_dir()
     if config_path is None:
-        config_path = (
-            root_dir
-            / "sentiment_classifier"
-            / "configs"
-            / "training"
-            / "config.cfg"
-        )
+        config_path = root_dir / configs["trainer"]["train_config"]
         config_path = str(config_path)
     if training_data_path is None:
-        training_data_path = root_dir / "data" / "train.spacy"
+        training_data_path = root_dir / configs["trainer"]["training_data_path"]
         training_data_path = str(training_data_path)
     if testing_data_path is None:
-        testing_data_path = root_dir / "data" / "test.spacy"
+        testing_data_path = root_dir / configs["trainer"]["testing_data_path"]
         testing_data_path = str(testing_data_path)
     if output_path is None:
-        output_path = root_dir / "models"
+        output_path = root_dir / configs["trainer"]["output_path"]
         output_path = str(output_path)
 
-    train(
-        config_path=config_path,
-        overrides={
-            "paths.train": training_data_path,
-            "paths.dev": testing_data_path,
-        },
-        output_path=output_path,
-    )
+    # train(
+    #     config_path=config_path,
+    #     overrides={
+    #         "paths.train": training_data_path,
+    #         "paths.dev": testing_data_path,
+    #     },
+    #     output_path=output_path,
+    # )
 
     spacy_model = load_spacy_model(model_path=Path(output_path) / "model-best")
 
@@ -64,15 +62,25 @@ def train_spacy_model(
     )
 
     experiment = get_or_create_experiment(
-        "spacy_classifier",
-        {"project_name": "sentiment_classifier", "framework": "spacy"},
+        mlflow_conf["experiment"]["name"],
+        mlflow_conf["experiment"]["tags"],
     )
+    client = mlflow.MlflowClient()
 
     with mlflow.start_run(
-        run_name="log_spacy_model", experiment_id=experiment.experiment_id
+        run_name=mlflow_conf["run"]["name"], experiment_id=experiment.experiment_id
     ) as run:
         mlflow.spacy.log_model(
             spacy_model=spacy_model,
-            artifact_path="best_model",
+            artifact_path=mlflow_conf["run"]["artifact_path"],
             signature=model_signature,
+            registered_model_name=mlflow_conf["run"]["registered_model_name"],
         )
+
+        registered_model = client.get_registered_model(name=mlflow_conf["run"]["registered_model_name"])
+        model_version = registered_model.latest_versions[0].version
+        
+        client.set_registered_model_alias(name=mlflow_conf["run"]["registered_model_name"], alias="Champion", version=model_version)
+        
+
+        
--- sentiment_classifier/sentiment_classifier/utils/utils.py
+++ sentiment_classifier/sentiment_classifier/utils/utils.py
@@ -1,7 +1,10 @@
 from pathlib import Path
-
+import pkgutil
 import spacy
+import yaml
 
+from typing import Any
+from typing import Dict
 
 def get_root_dir() -> Path:
     """
@@ -11,6 +14,16 @@ def get_root_dir() -> Path:
     """
     return Path(__file__).parent.parent.parent
 
+
+def read_config(name: str)-> Dict[str, Any]:
+    """
+    Read the configuration file.
+
+    :param name: The name of the configuration file.
+    """
+    data = pkgutil.get_data("sentiment_classifier", f"configs/{name}.yaml")
+    config = yaml.safe_load(data)
+    return config
 
 def load_spacy_model(model_path: str):
     """
