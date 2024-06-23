2347969496a9ff278af78e5a0270d54501a1ad80
Manuel Gil <manuelgilsitio@gmail.com>
2024-06-23 15:31:12 -0500

    Evaluating default shot classifier model

--- financial_news_sentiment/financial_news_sentiment/model/evaluation.py
+++ financial_news_sentiment/financial_news_sentiment/model/evaluation.py
@@ -1,15 +1,24 @@
+from typing import Any
 from typing import Dict
 from typing import List
+from typing import Optional
 
+import mlflow
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import balanced_accuracy_score
 from sklearn.metrics import f1_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import recall_score
 
+from financial_news_sentiment.model.utils import get_or_create_experiment
+
 
 def classification_metrics(
-    y_true: List[str], y_pred: List[str], prefix=""
+    y_true: List[str],
+    y_pred: List[str],
+    prefix: Optional[str] = "",
+    pos_label: Optional[str] = "positive",
+    labels: Optional[List[str]] = ["positive", "negative"],
 ) -> Dict[str, float]:
     """
     Calculate classification metrics.
@@ -21,9 +30,11 @@ def classification_metrics(
     """
 
     accuracy = accuracy_score(y_true, y_pred)
-    precision = precision_score(y_true, y_pred)
-    recall = recall_score(y_true, y_pred)
-    f1 = f1_score(y_true, y_pred)
+    precision = precision_score(
+        y_true, y_pred, labels=labels, pos_label=pos_label
+    )
+    recall = recall_score(y_true, y_pred, labels=labels, pos_label=pos_label)
+    f1 = f1_score(y_true, y_pred, labels=labels, pos_label=pos_label)
 
     return {
         f"{prefix}_accuracy": accuracy,
@@ -32,3 +43,39 @@ def classification_metrics(
         f"{prefix}_f1": f1,
         f"{prefix}_balanced_accuracy": balanced_accuracy_score(y_true, y_pred),
     }
+
+
+def track_run(
+    experiment_name: str,
+    experiment_tags: Optional[Dict[str, Any]] = None,
+    run_name: Optional[str] = None,
+    run_tags: Optional[Dict[str, Any]] = None,
+    metrics: Optional[Dict[str, float]] = None,
+    params: Optional[Dict[str, Any]] = None,
+    figures: Optional[Dict[str, Any]] = None,
+) -> None:
+    """
+    Track the run in MLflow.
+
+    :param experiment_name: Name of the experiment.
+    :param experiment_tags: Tags for the experiment.
+    :param run_name: Name of the run.
+    :param run_tags: Tags for the run.
+    :param metrics: Dictionary of metrics.
+    :param params: Dictionary of parameters.
+    :param figures: Dictionary of figures
+    :return: None
+    """
+
+    experiment_name = get_or_create_experiment(
+        name=experiment_name, tags=experiment_tags
+    )
+
+    with mlflow.start_run(run_name=run_name, tags=run_tags):
+        if metrics:
+            mlflow.log_metrics(metrics)
+        if params:
+            mlflow.log_params(params)
+        if figures:
+            for name, figure in figures.items():
+                mlflow.log_figure(figure, name)
--- financial_news_sentiment/financial_news_sentiment/model/predict.py
+++ financial_news_sentiment/financial_news_sentiment/model/predict.py
@@ -8,10 +8,13 @@ from transformers import pipeline
 
 from financial_news_sentiment.data.retrieval import get_train_test_data
 from financial_news_sentiment.data.retrieval import read_data
+from financial_news_sentiment.model.evaluation import classification_metrics
+from financial_news_sentiment.model.evaluation import track_run
 from financial_news_sentiment.model.pipelines import ShotClassifier
 from financial_news_sentiment.utils.utils import add_encoding
 from financial_news_sentiment.utils.utils import as_dataframe
 from financial_news_sentiment.utils.utils import get_root_path
+from financial_news_sentiment.utils.utils import read_config
 
 
 def get_prediction(texts: List[str], labels: List[str] = None):
@@ -56,14 +59,55 @@ def predict2():
     """
 
     df = read_data()
-    train_df, _ = get_train_test_data(df)
-    texts = train_df["text"].iloc[0:10].tolist()
+    # transform the data to a binary classification problem
+    df["sentiment"] = df["sentiment"].apply(
+        lambda x: "positive" if x == "positive" else "negative"
+    )
+    print(df["sentiment"].value_counts())
 
+    _, test_df = get_train_test_data(df)
     classifier = ShotClassifier()
-    predictions = classifier.predict(texts)
-    print(predictions)
+    predictions = classifier.predict(test_df["text"].to_list())
     predictions_df = as_dataframe(predictions)
+    predictions_df["sentiment"] = test_df["sentiment"].to_list()
+
+    root = get_root_path()
+    output_path = (
+        root
+        / "financial_news_sentiment"
+        / "output"
+        / "shot_classifier_test_dataset_predictions.csv"
+    )
+    output_path = add_encoding(output_path)
+    predictions_df.to_csv(output_path, index=False)
     print(predictions_df.head())
+
+
+def track_experiment(experiment_config: str):
+    """
+    Track the experiment in MLflow.
+    """
+    config = read_config(experiment_config)
+
+    experiment_name = config["experiment_name"]
+    experiment_tags = config["experiment_tags"]
+    run_name = config["run_name"]
+    run_tags = config["run_tags"]
+    data_path = config["dataset_path"]
+    df = pd.read_csv(data_path)
+    metrics = classification_metrics(
+        df["label"], df["sentiment"], prefix="test"
+    )
+    track_run(
+        experiment_name, experiment_tags, run_name, run_tags, metrics=metrics
+    )
+
+
+def log_experiments():
+    """
+    Log experiments in MLflow.
+    """
+    track_experiment("experiments/shot_classifier")
 
 
 def predict():
